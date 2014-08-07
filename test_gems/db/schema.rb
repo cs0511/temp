7fac47a461f14a31de46d42fd3ce08dcd85758f3
Song Chen <chens@shinetechchina.com>
2014-08-07 21:48:18 +0800

    add user login

--- test_gems/db/schema.rb
+++ test_gems/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803091327) do
+ActiveRecord::Schema.define(version: 20140804122115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
