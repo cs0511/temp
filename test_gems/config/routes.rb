52361d8778381e6ac706dad85c4fc32aafc31070
Song Chen <chens@shinetechchina.com>
2014-08-12 16:14:03 +0800

    add paypal button

--- test_gems/config/routes.rb
+++ test_gems/config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :tutorial
+  resources :checkout
   # Example resource route with options:
   #   resources :products do
   #     member do
